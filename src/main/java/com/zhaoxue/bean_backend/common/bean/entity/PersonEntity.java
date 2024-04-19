9225a29ba9551254916fcbe284163dfc53de53c2
zhaoxue <fromzhaoxue@gmail.com>
2024-04-20 01:25:16 +0900

    udpate entity

--- src/main/java/com/zhaoxue/bean_backend/common/bean/entity/PersonEntity.java
+++ src/main/java/com/zhaoxue/bean_backend/common/bean/entity/PersonEntity.java
@@ -6,15 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "person")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class PersonEntity {
 
 	@Id
@@ -23,4 +17,30 @@ public class PersonEntity {
 
 	@Column(name = "name", nullable = false, length = 50)
 	private String name;
+
+	// 默认构造函数
+	public PersonEntity() {
+	}
+
+	// 带参数的构造函数
+	public PersonEntity(String name) {
+		this.name = name;
+	}
+
+	// Getter和Setter方法
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- src/main/java/com/zhaoxue/bean_backend/common/service/PersonService.java
+++ src/main/java/com/zhaoxue/bean_backend/common/service/PersonService.java
@@ -1,7 +1,10 @@
 package com.zhaoxue.bean_backend.common.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import com.zhaoxue.bean_backend.common.bean.entity.PersonEntity;
 import com.zhaoxue.bean_backend.common.bean.repository.PersonRepository;
@@ -9,18 +12,26 @@ import com.zhaoxue.bean_backend.common.bean.repository.PersonRepository;
 @Service
 public class PersonService {
 
+	private static final Logger logger = LoggerFactory.getLogger(PersonService.class);
+
 	@Autowired
 	private PersonRepository personRepository;
 
 	/**
 	 * 添加新的Person对象到数据库
 	 * 
-	 * @param name 人名
+	 * @param name 人名，不应为空
 	 * @return 保存后的Person对象
 	 */
 	public PersonEntity createPerson(String name) {
+		Assert.hasText(name, "Name must not be empty");
 		PersonEntity person = new PersonEntity(name);
-		return personRepository.save(person);
+		try {
+			return personRepository.save(person);
+		} catch (Exception e) {
+			logger.error("Error saving person: ", e);
+			throw e;
+		}
 	}
 
 	/**
@@ -29,7 +40,8 @@ public class PersonService {
 	 * @param id 人的ID
 	 * @return 返回找到的name，如果没有找到返回null
 	 */
-	public String findNameById(int id) {
-		return personRepository.findById(id).map(PersonEntity::getName).orElse(null); // 如果未找到，返回null
+	public String findNameById(Long id) {
+		Assert.notNull(id, "ID must not be null");
+		return personRepository.findById(id).map(PersonEntity::getName).orElse(null);
 	}
 }

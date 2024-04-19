b0c25210530798b50c58d6e7c3698ee7ca03d119
zhaoxue <fromzhaoxue@gmail.com>
2024-04-20 01:15:43 +0900

    update controller

--- src/main/java/com/zhaoxue/bean_backend/controller/PersonController.java
+++ src/main/java/com/zhaoxue/bean_backend/controller/PersonController.java
@@ -1,12 +1,14 @@
 package com.zhaoxue.bean_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zhaoxue.bean_backend.common.bean.entity.PersonEntity;
+import com.zhaoxue.bean_backend.common.bean.repository.PersonEntity;
 import com.zhaoxue.bean_backend.common.service.PersonService;
 
 @RestController
@@ -16,12 +18,13 @@ public class PersonController {
 	private PersonService personService;
 
 	@PostMapping("/persons")
+	@ResponseStatus(HttpStatus.CREATED)
 	public PersonEntity addPerson(@RequestParam String name) {
 		return personService.createPerson(name); // 插入新的Person对象
 	}
 
 	@GetMapping("/persons/name")
-	public String getNameById(@RequestParam int id) {
+	public String getNameById(@RequestParam Long id) {
 		return personService.findNameById(id); // 根据id查询name
 	}
 }

e8ae0e775d226ff26e86a8076021f604399aadc0
zhaoxue <fromzhaoxue@gmail.com>
2024-04-20 01:36:58 +0900

    udpate service

--- src/main/java/com/zhaoxue/bean_backend/common/service/PersonService.java
+++ src/main/java/com/zhaoxue/bean_backend/common/service/PersonService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import com.zhaoxue.bean_backend.common.bean.entity.PersonEntity;
@@ -18,11 +19,12 @@ public class PersonService {
 	private PersonRepository personRepository;
 
 	/**
-	 * 添加新的Person对象到数据库
+	 * 添加新的Person对象到数据库 这个操作涉及到数据的修改，因此我们声明了 @Transactional，以确保操作的原子性。
 	 * 
 	 * @param name 人名，不应为空
 	 * @return 保存后的Person对象
 	 */
+	@Transactional
 	public PersonEntity createPerson(String name) {
 		Assert.hasText(name, "Name must not be empty");
 		PersonEntity person = new PersonEntity(name);
@@ -35,11 +37,12 @@ public class PersonService {
 	}
 
 	/**
-	 * 根据id查找person的name
+	 * 根据id查找person的name 因为这是一个只读的操作，我们使用 @Transactional(readOnly = true) 来优化事务处理。
 	 * 
-	 * @param id 人的ID
+	 * @param id 人的ID，不应为空
 	 * @return 返回找到的name，如果没有找到返回null
 	 */
+	@Transactional(readOnly = true)
 	public String findNameById(Long id) {
 		Assert.notNull(id, "ID must not be null");
 		return personRepository.findById(id).map(PersonEntity::getName).orElse(null);
